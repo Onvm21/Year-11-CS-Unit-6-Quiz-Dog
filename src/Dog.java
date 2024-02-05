e4ecb4e653a533384778a8af3fa8d17b41d1ebbb
Onvm2007 <nicks2026@student.cis.edu.hk>
2024-02-05 08:26:53 +0800

    done

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,52 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String colour;
+    private Owner owner;
+
+    public Dog(int age, String name, String colour, Owner owner){
+        this.age = age;
+        this.name = name;
+        this.colour = colour;
+        this.owner = owner;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getColour(){
+        return colour;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setColour(String colour){
+        this.colour = colour;
+    }
+    public void setOwner(Owner owner){
+        this.owner = owner;
+    }
+    private void addOneAge(){
+        age = age + 1;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    public String toString(){
+        return("The dog is named " + name + " and is " + age + " years old. This dog is " + colour + ".");
+    }
+
 }
+
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,34 @@
 public class Owner {
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+
+    public Owner (String firstName, String lastName, int phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
 }
+
